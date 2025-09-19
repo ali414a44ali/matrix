a4883c03cefa3bd0d6669ec1d56492edbade9f63
BiLaL <97440505+E9N99@users.noreply.github.com>
2025-09-19 18:52:27 +0300

    Add files via upload

---
+++ Matrix/plugins/بحث صور.py
@@ -0,0 +1,155 @@
+# dev-Thon - ZelZal
+# Copyright (C) 2022 BiLaL . All Rights Reserved
+#
+# This file is a part of < https://github.com/dev-Thon/ZelZal/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/dev-Thon/ZelZal/blob/master/LICENSE/>.
+
+#الملف محمي بحقوق الملكيـه الخـاصه بـ GNU Affero General Public License
+#So تخمـط الملف ابلـع سـورسك بانـد
+
+import asyncio
+import aiohttp
+import os
+import shutil
+import time
+from bs4 import BeautifulSoup
+from datetime import datetime
+from telethon.utils import guess_extension
+from urllib.parse import urlencode
+
+from Matrix import blal
+from ..Config import Config
+
+ZELZAL_APP_ID = "6e65179ed1d879f3d905e28ef8803625"
+
+
+@blal.dev_cmd(pattern="صور (.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    await event.edit("**╮ ❐ جـاري البحث عن الصـور  ...𓅫╰**")
+    BiLaL = event.pattern_match.group(1)
+    wdev_dir = os.path.join(
+        Config.TMP_DOWNLOAD_DIRECTORY,
+        BiLaL
+    )
+    if not os.path.isdir(wdev_dir):
+        os.makedirs(wdev_dir)
+    input_url = "https://bots.shrimadhavuk.me/search/"
+    headers = {"USER-AGENT": "UseTGBot"}
+    url_lst = []
+    async with aiohttp.ClientSession() as requests:
+        data = {
+            "q": BiLaL,
+            "app_id": ZELZAL_APP_ID,
+            "p": "GoogleImages"
+        }
+        reponse = await requests.get(
+            input_url,
+            params=data,
+            headers=headers
+        )
+        response = await reponse.json()
+        for result in response["results"]:
+            if len(url_lst) > 9:
+                break
+            caption = result.get("description")
+            image_url = result.get("url")
+            image_req_set = await requests.get(image_url)
+            image_file_name = str(time.time()) + "" + guess_extension(
+                image_req_set.headers.get("Content-Type")
+            )
+            image_save_path = os.path.join(
+                wdev_dir,
+                image_file_name
+            )
+            with open(image_save_path, "wb") as f_d:
+                f_d.write(await image_req_set.read())
+            url_lst.append(image_save_path)
+    if not url_lst:
+        await event.edit(f"**- اووبـس .. لم استطـع ايجـاد صـور عـن {BiLaL} ؟!**\n**- حـاول مجـددًا واكتـب الكلمـة بشكـل صحيح**")
+        return
+    await event.reply(
+        file=url_lst,
+        parse_mode="html",
+        force_document=True
+    )
+    for each_file in url_lst:
+        os.remove(each_file)
+    shutil.rmtree(wdev_dir, ignore_errors=True)
+    end = datetime.now()
+    ms = (end - start).seconds
+    await event.edit(
+        f"**- اكتمـل البحث عـن {BiLaL} في {ms} ثانيـة ✓**",
+        link_preview=False
+    )
+    await asyncio.sleep(5)
+    await event.delete()
+
+
+
+@blal.dev_cmd(pattern="خلفيات (.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    await event.edit("**╮ ❐ جـاري البحث عن خلفيـات  ...𓅫╰**")
+    BiLaL = event.pattern_match.group(1)
+    wdev_dir = os.path.join(
+        Config.TMP_DOWNLOAD_DIRECTORY,
+        BiLaL
+    )
+    if not os.path.isdir(wdev_dir):
+        os.makedirs(wdev_dir)
+    input_url = "https://bots.shrimadhavuk.me/search/"
+    headers = {"USER-AGENT": "UseTGBot"}
+    url_lst = []
+    async with aiohttp.ClientSession() as requests:
+        data = {
+            "q": BiLaL,
+            "app_id": ZELZAL_APP_ID,
+            "p": "GoogleImages"
+        }
+        reponse = await requests.get(
+            input_url,
+            params=data,
+            headers=headers
+        )
+        response = await reponse.json()
+        for result in response["results"]:
+            if len(url_lst) > 9:
+                break
+            caption = result.get("description")
+            image_url = result.get("url")
+            image_req_set = await requests.get(image_url)
+            image_file_name = str(time.time()) + "" + guess_extension(
+                image_req_set.headers.get("Content-Type")
+            )
+            image_save_path = os.path.join(
+                wdev_dir,
+                image_file_name
+            )
+            with open(image_save_path, "wb") as f_d:
+                f_d.write(await image_req_set.read())
+            url_lst.append(image_save_path)
+    if not url_lst:
+        await event.edit(f"**- اووبـس .. لم استطـع ايجـاد خلفيـات عـن {BiLaL} ؟!**\n**- حـاول مجـددًا واكتـب الكلمـة بشكـل صحيح**")
+        return
+    await event.reply(
+        file=url_lst,
+        parse_mode="html",
+        force_document=True
+    )
+    for each_file in url_lst:
+        os.remove(each_file)
+    shutil.rmtree(wdev_dir, ignore_errors=True)
+    end = datetime.now()
+    ms = (end - start).seconds
+    await event.edit(
+        f"**- اكتمـل البحث عـن {BiLaL} في {ms} ثانيـة ✓**",
+        link_preview=False
+    )
+    await asyncio.sleep(5)
+    await event.delete()
